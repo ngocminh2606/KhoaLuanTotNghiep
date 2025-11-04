a894db609aab266709dc3f6993255b1e8d74ae84
ngocminh2606 <23020851@vnu.edu.vn>
2025-11-04 11:03:32 +0700

    Thêm lớp Advisor: lưu thông tin người hướng dẫn

---
+++ Advisor.java
@@ -0,0 +1,25 @@
+public class Advisor {
+    private String fullname;
+    private String email;
+
+    public Advisor(String fullname, String email) {
+        this.fullname = fullname;
+        this.email = email;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Advisor{" +
+                "fullname='" + fullname + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
